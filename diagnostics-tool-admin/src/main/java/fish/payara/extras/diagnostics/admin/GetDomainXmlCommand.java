c5f3abf1adb01a9054686feedc730163dc1a8d18
Kalin Chan <kalin.chan@payara.fish>
2024-03-21 12:40:36 +0000

    latest

--- diagnostics-tool-admin/src/main/java/fish/payara/extras/diagnostics/admin/GetDomainXmlCommand.java
+++ diagnostics-tool-admin/src/main/java/fish/payara/extras/diagnostics/admin/GetDomainXmlCommand.java
@@ -1,26 +1,34 @@
 package fish.payara.extras.diagnostics.admin;
 
+import com.sun.enterprise.config.serverbeans.Server;
 import org.glassfish.api.ActionReport;
 import org.glassfish.api.Param;
-import org.glassfish.api.admin.AdminCommand;
-import org.glassfish.api.admin.AdminCommandContext;
-import org.glassfish.api.admin.CommandLock;
-import org.glassfish.api.admin.ExecuteOn;
-import org.glassfish.api.admin.FailurePolicy;
-import org.glassfish.api.admin.RuntimeType;
-import org.glassfish.api.admin.ServerEnvironment;
+import org.glassfish.api.admin.*;
 import org.glassfish.config.support.CommandTarget;
 import org.glassfish.config.support.TargetType;
 import org.glassfish.hk2.api.PerLookup;
 import org.jvnet.hk2.annotations.Service;
 
 import javax.inject.Inject;
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Service(name = "get-domain-xml")
 @PerLookup
 @CommandLock(CommandLock.LockType.NONE)
 @TargetType({CommandTarget.DAS, CommandTarget.STANDALONE_INSTANCE, CommandTarget.CLUSTERED_INSTANCE})
 @ExecuteOn(value = {RuntimeType.INSTANCE}, ifNeverStarted = FailurePolicy.Error)
+@RestEndpoints({
+        @RestEndpoint(configBean = Server.class,
+                opType = RestEndpoint.OpType.GET,
+                path = "list-certificates",
+                description = "List Keys and Certificates in key or trust store",
+                params = {
+                        @RestParam(name = "target", value = "$parent")
+                }
+        )
+})
 public class GetDomainXmlCommand implements AdminCommand {
 
     @Param(name = "target", optional = true)
@@ -32,7 +40,16 @@ public class GetDomainXmlCommand implements AdminCommand {
     @Override
     public void execute(AdminCommandContext adminCommandContext) {
         ActionReport report = adminCommandContext.getActionReport();
-        report.setMessage(serverEnvironment.getInstanceRoot().toString());
+        Path domainXMLPath = Paths.get(serverEnvironment.getInstanceRoot().getAbsolutePath(), "config", "domain.xml");
+        File domainXml = domainXMLPath.toFile();
+
+        if (!domainXml.exists()) {
+            String result = String.format("domain.xml not found at %s", domainXMLPath);
+            report.setMessage(result);
+            report.setActionExitCode(ActionReport.ExitCode.FAILURE);
+        }
+
+        report.setMessage("Found at " + domainXMLPath);
         report.setActionExitCode(ActionReport.ExitCode.SUCCESS);
     }
 }
